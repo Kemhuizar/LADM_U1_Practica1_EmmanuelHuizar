3c484caa9c2c717fcb2069b70cb5881ccb7a0bf3
Kemhuizar <keemhuizarag@ittepic.edu.mx>
2020-02-12 07:54:15 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_emmanuelhuizar/Main5Activity.kt
@@ -0,0 +1,17 @@
+package mx.edu.ittepic.ladm_u1_practica1_emmanuelhuizar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main5.*
+
+class Main5Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main5)
+
+        button6.setOnClickListener {
+
+        }
+    }
+}
